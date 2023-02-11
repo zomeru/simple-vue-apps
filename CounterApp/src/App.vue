1f3d75fedb1e67e224936206b0a22c1a161c0f95
zomeru <zomerzxc.19@gmail.com>
2023-02-11 16:31:47 +0800

    Add counterapp and noteapp

---
+++ CounterApp/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const count = ref(0);
+
+  const increment = () => {
+    count.value = count.value + 1;
+  };
+
+  const decrement = () => {
+    if (count.value > 0) {
+      count.value = count.value - 1;
+    } else {
+      alert('You cannot go below 0');
+    }
+  };
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="decrement">-</button>
+      <button @click="increment">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main {
+    background-color: aliceblue;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  div {
+    text-align: center;
+  }
+</style>
